f0693fa2442ae8bf7ccaaca703cbd9ab5dafa47f
TheAliBigdeli <bigdeli.ali3@gmail.com>
2021-06-16 11:28:07 +0430

    created simple models for blog and website
    added website contact model
    added blog models including:
    +tag
    +post
    +category

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.CharField(max_length=255,null=True)
+    # image = 
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.CharField(max_length=255,null=True)
+    tag = models.CharField(max_length=255,null=True)
+    counted_view = models.IntegerField(default=0)
+    published_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    messages = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
