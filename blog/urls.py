7bf9b4b774849db408adb0a43e5e373e4691cf26
TheAliBigdeli <bigdeli.ali3@gmail.com>
2021-06-29 21:19:21 +0430

    added category and user filter
    changes pages and added some features
    pagination
    template tags
    and so

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,8 @@ app_name = 'blog'
 urlpatterns = [
     path('' ,blog_view,name='index'),
     path('<int:pid>' ,blog_single,name='single'),
+    path('category/<str:cat_name>' ,blog_view,name='category'),
+    path('author/<str:author_username>',blog_view,name='author'),
+    path('search/',blog_search,name='search'),
     path('test',test,name='test'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
-def blog_view(request):
+def blog_view(request,**kwargs):
     posts = Post.objects.filter(status=1)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username = kwargs['author_username'])
+    posts = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
@@ -12,5 +25,17 @@ def blog_single(request,pid):
     context = {'post':post}
     return render(request,'blog/blog-single.html',context)
 
-def test(request):    
+def test(request):
     return render(request,'test.html')
+
+
+def blog_search(request):
+    #print(request.__dict__)
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        #print(request.GET.get('s'))
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+    
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
